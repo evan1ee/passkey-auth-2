f37c52ecca5984cef7593d1342c18662783b0bca
Evan11e <evan.1ee@outlook.com>
2025-02-13 11:49:59 +0800

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import crypto from "crypto";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -8,21 +7,3 @@ export function cn(...inputs: ClassValue[]) {
 
 
 
-function clean(str: string) {
-  return str.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
-}
-
-function binaryToBase64url(bytes: Uint8Array) {
-  let str = "";
-
-  bytes.forEach((charCode) => {
-    str += String.fromCharCode(charCode);
-  });
-
-  return btoa(str);
-}
-
-
-export function generateChallenge() {
-  return clean(crypto.randomBytes(32).toString("base64"));
-}
